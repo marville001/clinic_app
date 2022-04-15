e91241116e8412ce9a6db962748239474b9e5b3b
Martin Mwangi <mwangimartin1904@gmail.com>
2022-04-15 14:02:20 +0300

    Updates

--- src/redux/reducers/auth.reducer.js
+++ src/redux/reducers/auth.reducer.js
@@ -1,30 +1,35 @@
 import { USER_LOGIN, LOGOUT_USER } from "../types";
 
 const initialState = {
-  authDetails: {},
-  loading: false,
-  error: "",
+    authDetails: {},
+    loading: false,
+    error: "",
 };
 
 const authReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case USER_LOGIN.REQUEST:
-      return { ...state, login: true, user: {}, error: "" };
-    case USER_LOGIN.SUCCESS:
-      return {
-        ...state,
-        authDetails: action.payload,
-        loading: false,
-        error: "",
-      };
-    case USER_LOGIN.FAIL:
-      return { ...state, login: false, error: action.payload };
-    case LOGOUT_USER:
-      return { ...state, user: {}, loading: false, error:"" };
+    switch (action.type) {
+        case USER_LOGIN.REQUEST:
+            return { ...state, login: true, authDetails: {}, error: "" };
+        case USER_LOGIN.SUCCESS:
+            return {
+                ...state,
+                authDetails: action.payload,
+                loading: false,
+                error: "",
+            };
+        case USER_LOGIN.FAIL:
+            return {
+                ...state,
+                authDetails: {},
+                login: false,
+                error: action.payload,
+            };
+        case LOGOUT_USER:
+            return { ...state, authDetails: {}, loading: false, error: "" };
 
-    default:
-      return { ...state };
-  }
+        default:
+            return { ...state };
+    }
 };
 
 export default authReducer;
