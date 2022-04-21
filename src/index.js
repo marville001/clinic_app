8b4f68445ceca6b9819a8195433c036ebaebc323
Martin Mwangi <mwangimartin1904@gmail.com>
2022-04-21 17:32:18 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -11,13 +11,11 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <NavProvider>
-                <Router>
-                    <App />
-                </Router>
-            </NavProvider>
-        </Provider>
-    </React.StrictMode>
+    <Provider store={store}>
+        <NavProvider>
+            <Router>
+                <App />
+            </Router>
+        </NavProvider>
+    </Provider>
 );
