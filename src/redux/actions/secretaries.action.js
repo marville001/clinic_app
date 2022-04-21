669de46e1bb700444642824a24076de93ee267c6
Martin Mwangi <mwangimartin1904@gmail.com>
2022-04-21 10:47:55 +0300

    Sec actions done

--- src/redux/actions/secretaries.action.js
+++ src/redux/actions/secretaries.action.js
@@ -1,5 +1,17 @@
-import { createSecretaryApi } from "../../api/secretaries";
-import { CREATE_SECRETARY } from "../types/secretaries.types";
+import {
+    createSecretaryApi,
+    deleteSecretaryApi,
+    getSecretariesApi,
+    getSecretaryApi,
+    updateSecretaryApi,
+} from "../../api/secretaries";
+import {
+    CREATE_SECRETARY,
+    DELETE_SECRETARY,
+    GET_SECRETARIES,
+    GET_SECRETARY,
+    UPDATE_SECRETARY,
+} from "../types/secretaries.types";
 
 export const createSecretaryAction = (user) => async (dispatch) => {
     dispatch({ type: CREATE_SECRETARY.REQUEST });
@@ -11,17 +23,99 @@ export const createSecretaryAction = (user) => async (dispatch) => {
         });
         return { success: true };
     } catch (error) {
+        const err =
+            error?.response?.data?.message ||
+            "An error occurred. Please try again";
         dispatch({
             type: CREATE_SECRETARY.FAIL,
+            payload: err,
+        });
+        return {
+            success: false,
+            message: err,
+        };
+    }
+};
+
+export const getSecretaryAction = (id) => async (dispatch) => {
+    dispatch({ type: GET_SECRETARY.REQUEST });
+    try {
+        const { data } = await getSecretaryApi(id);
+        dispatch({
+            type: GET_SECRETARY.SUCCESS,
+            payload: data.secretary,
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_SECRETARY.FAIL,
             payload:
                 error?.response?.data?.message ||
                 "An error occurred. Please try again",
         });
-        return {
-            success: false,
-            message:
+    }
+};
+
+export const getSecretariesAction = () => async (dispatch) => {
+    dispatch({ type: GET_SECRETARIES.REQUEST });
+    try {
+        const { data } = await getSecretariesApi();
+        dispatch({
+            type: GET_SECRETARIES.SUCCESS,
+            payload: data.secretaries,
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_SECRETARIES.FAIL,
+            payload:
                 error?.response?.data?.message ||
                 "An error occurred. Please try again",
+        });
+    }
+};
+
+export const updateSecretaryAction = (details, id) => async (dispatch) => {
+    dispatch({ type: UPDATE_SECRETARY.REQUEST });
+    try {
+        const { data } = await updateSecretaryApi(details, id);
+        dispatch({
+            type: UPDATE_SECRETARY.SUCCESS,
+            payload: data.secretary,
+        });
+        return { success: true };
+    } catch (error) {
+        const err =
+            error?.response?.data?.message ||
+            "An error occurred. Please try again";
+        dispatch({
+            type: UPDATE_SECRETARY.FAIL,
+            payload: err,
+        });
+        return {
+            success: false,
+            message: err,
+        };
+    }
+};
+
+export const deleteSecretaryAction = (id) => async (dispatch) => {
+    dispatch({ type: DELETE_SECRETARY.REQUEST });
+    try {
+        await deleteSecretaryApi(id);
+        dispatch({
+            type: DELETE_SECRETARY.SUCCESS,
+        });
+        return { success: true };
+    } catch (error) {
+        const err =
+            error?.response?.data?.message ||
+            "An error occurred. Please try again";
+        dispatch({
+            type: DELETE_SECRETARY.FAIL,
+            payload: err,
+        });
+        return {
+            success: false,
+            message: err,
         };
     }
 };
