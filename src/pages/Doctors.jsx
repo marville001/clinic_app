e1a7b352c18c2d5b51851766bd4755358b9771ed
Martin Mwangi <mwangimartin1904@gmail.com>
2022-04-23 23:35:15 +0300

    Add doctor

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -3,15 +3,22 @@ import { Link } from "react-router-dom";
 import DashboardWrapper from "../components/DashboardWrapper";
 import Header from "../components/Header";
 import SearchInput from "../components/SearchInput";
-import { FaEye, FaTrash, FaUserEdit } from "react-icons/fa";
+import { FaEye, FaSpinner, FaTrash, FaUserEdit } from "react-icons/fa";
 import { HiPlusCircle } from "react-icons/hi";
 import ConfirmDeleteModal from "../components/modals/ConfirmDeleteModal";
 import AddDepartmentModal from "../components/modals/AddDepartmentModal";
 import { useDispatch, useSelector } from "react-redux";
 import { getDepartmentsAction } from "../redux/actions/departments.action";
+import { getDoctorsAction } from "../redux/actions/doctors.action";
 
 const Doctors = () => {
-    const {departments} = useSelector(state=>state.departmentsState)
+    const { departments, loading: loading_dpt } = useSelector(
+        (state) => state.departmentsState
+    );
+    const { authDetails } = useSelector((state) => state.authState);
+    const { doctors, loading: loading_dct } = useSelector(
+        (state) => state.doctorsState
+    );
     const [addModalOpen, setAddModalOpen] = useState(false);
     const [confirmDeleteModalOpen, setConfirmDeleteModalOpen] = useState(false);
 
@@ -22,22 +29,27 @@ const Doctors = () => {
     };
 
     const handleDeleteDoctor = () => {
-        console.log("Deleting");
         setConfirmDeleteModalOpen(false);
     };
 
     useEffect(() => {
-      dispatch(getDepartmentsAction())
-    }, [dispatch])
-    
+        authDetails?._id && dispatch(getDepartmentsAction());
+    }, [dispatch, authDetails?._id]);
+
+    useEffect(() => {
+        authDetails?._id && dispatch(getDoctorsAction());
+    }, [dispatch, authDetails?._id]);
 
     return (
         <DashboardWrapper>
             <Header title="Doctors" />
             <div className="p-4 ">
                 <div className="my-6 p-4 bg-white _shadow">
-                    <div className="flex justify-between items-center">
+                    <div className="flex gap-5 items-center">
                         <h2 className="font-bold text-lg">Departments</h2>
+                        {loading_dpt && (
+                            <FaSpinner className="animate-spin text-slate-900" />
+                        )}
                         {/* <div
                             className="flex items-center space-x-2 py-2 cursor-pointer px-6 rounded-md text-seagreen  text-sm hover:opacity-75"
                         >
@@ -84,6 +96,7 @@ const Doctors = () => {
                                     <tr>
                                         <th className="px-6 py-4">#</th>
                                         <th className="px-6 py-4">Full Name</th>
+                                        <th className="px-6 py-4">Email</th>
                                         <th className="px-6 py-4">Gender</th>
                                         <th className="px-6 py-4">DOB</th>
                                         <th className="px-6 py-4">Address</th>
@@ -101,60 +114,76 @@ const Doctors = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {[1, 2, 3, 4, 5, 6].map((doctor, idx) => (
-                                        <tr
-                                            key={doctor}
-                                            className="group border-b "
-                                        >
-                                            <td className="px-6 py-4 font-bold">
-                                                {idx + 1}
+                                    {loading_dct ? (
+                                        <tr>
+                                            <td colSpan={5} className="py-5 flex justify-center">
+                                                <FaSpinner className="animate-spin  text-lg text-slate-900" />
                                             </td>
-                                            <td className="px-6 py-4">
-                                                Alfred Sliver
-                                            </td>
-                                            <td className="px-6 py-4">Male</td>
-                                            <td className="px-6 py-4">
-                                                27<sup>th</sup> June 2000
-                                            </td>
-                                            <td className="px-6 py-4">
-                                                1451 - ABC Street, NY
-                                            </td>
-                                            <td className="px-6 py-4">
-                                                +6575 563535353
-                                            </td>
-                                            <td className="px-6 py-4">
-                                                Dentist
-                                            </td>
-                                            <td className="px-6  py-4 text-right flex justify-end items-center space-x-1">
-                                                <Link
-                                                    className="
+                                        </tr>
+                                    ) : (
+                                        doctors?.map((doctor, idx) => (
+                                            <tr
+                                                key={doctor._id}
+                                                className="group border-b "
+                                            >
+                                                <td className="px-6 py-4 font-bold">
+                                                    {idx + 1}
+                                                </td>
+                                                <td className="px-6 py-4">
+                                                    {doctor?.firstname}{" "}
+                                                    {doctor?.lastname}
+                                                </td>
+                                                <td className="px-6 py-4">
+                                                    {doctor?.email}
+                                                </td>
+                                                <td className="px-6 py-4">
+                                                    {doctor?.gender}
+                                                </td>
+                                                <td className="px-6 py-4">
+                                                    {doctor?.dob || "-"}
+                                                </td>
+                                                <td className="px-6 py-4">
+                                                    {doctor?.address || "-"}
+                                                </td>
+                                                <td className="px-6 py-4">
+                                                    {doctor?.phone || "-"}
+                                                </td>
+                                                <td className="px-6 py-4">
+                                                    {doctor?.department || "-"}
+                                                </td>
+                                                <td className="px-6  py-4 text-right flex justify-end items-center space-x-1">
+                                                    <Link
+                                                        className="
                                                 flex items-center space-x-1 bg-seagreen text-white 
                                                 text-xs p-2 rounded-full hover:opacity-90 
                                                 hover:scale-[1.02] "
-                                                    to="/doctors/23fsr34few433d342"
-                                                >
-                                                    <FaEye />
-                                                </Link>
+                                                        to={`/doctors/${doctor?._id}`}
+                                                    >
+                                                        <FaEye />
+                                                    </Link>
 
-                                                <Link
-                                                    className="flex items-center space-x-1 bg-dimgray text-white text-xs p-2 
+                                                    <Link
+                                                        className="flex items-center space-x-1 bg-dimgray text-white text-xs p-2 
                                                 rounded-full hover:opacity-90 hover:scale-[1.02]"
-                                                    to="/doctors/23fsr34few433d342/edit"
-                                                >
-                                                    <FaUserEdit />
-                                                </Link>
+                                                        to={`/doctors/${doctor?._id}/edit`}
+                                                    >
+                                                        <FaUserEdit />
+                                                    </Link>
 
-                                                <div
-                                                    onClick={() =>
-                                                        openDeleteModal(idx)
-                                                    }
-                                                    className="flex items-center space-x-1 bg-salmon text-white text-xs p-2 rounded-full cursor-pointer hover:opacity-90 hover:scale-[1.02]"
-                                                >
-                                                    <FaTrash />
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    ))}
+                                                    <div
+                                                        onClick={() =>
+                                                            openDeleteModal(
+                                                                doctor?._id
+                                                            )
+                                                        }
+                                                        className="flex items-center space-x-1 bg-salmon text-white text-xs p-2 rounded-full cursor-pointer hover:opacity-90 hover:scale-[1.02]"
+                                                    >
+                                                        <FaTrash />
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    )}
                                 </tbody>
                             </table>
                         </div>
