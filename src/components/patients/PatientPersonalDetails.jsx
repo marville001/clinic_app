d89bae068cba9fe556d4d6b33036f073ecb3056c
Martin Mwangi <mwangimartin1904@gmail.com>
2022-04-25 22:36:25 +0300

    :sparkles: show doctor profile details

--- src/components/patients/PatientPersonalDetails.jsx
+++ src/components/patients/PatientPersonalDetails.jsx
@@ -9,9 +9,7 @@ const PatientPersonalDetails = () => {
         <div className="p-4 flex-[1] xl:flex-[2] rounded bg-white _shadow self-stfart">
             <h3 className="text-md mb-4 font-bold">Personal Information</h3>
             <p className="text-sm my-2">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
-                distinctio vero non libero nisi in voluptatibus atque, ut alias
-                ipsum?
+                {patient?.bio}
             </p>
 
             <div className="flex w-full sm:w-3/4 my-5">
