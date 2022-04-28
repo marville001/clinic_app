d5bece96cb43cf8fc548b80cf43d38eba4032c96
Martin Mwangi <mwangimartin1904@gmail.com>
2022-04-28 19:46:55 +0300

    :sparkles: show admins done

--- src/pages/Admins.jsx
+++ src/pages/Admins.jsx
@@ -1,14 +1,195 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FaEye, FaSpinner, FaTrash, FaUserEdit } from "react-icons/fa";
+import { HiPlusCircle } from "react-icons/hi";
+import { useDispatch, useSelector } from "react-redux";
 import DashboardWrapper from "../components/DashboardWrapper";
 import Header from "../components/Header";
+import ConfirmDeleteModal from "../components/modals/ConfirmDeleteModal";
 import SearchInput from "../components/SearchInput";
+import {
+    deleteAdminAction,
+    getAdminsAction,
+} from "../redux/actions/admins.action";
+
+import { toast } from "react-toastify";
 
 const Admins = () => {
+    const { authDetails } = useSelector((state) => state.authState);
+    const {
+        loading: loading_adm,
+        admins,
+        deleting: deleting_adm,
+    } = useSelector((state) => state.adminsState);
+
+    const [addModalOpen, setAddModalOpen] = useState(false);
+    const [editModalOpen, setEditModalOpen] = useState(false);
+    const [viewModalOpen, setViewModalOpen] = useState(false);
+    const [selectedAdmin, setSelectedAdmin] = useState({});
+    const [confirmDeleteModalOpen, setConfirmDeleteModalOpen] = useState(false);
+
+    const dispatch = useDispatch();
+
+    const openEditModal = (admin) => {
+        setSelectedAdmin(admin);
+        setEditModalOpen(true);
+    };
+
+    const openViewModal = (admin) => {
+        setSelectedAdmin(admin);
+        setViewModalOpen(true);
+    };
+
+    const openDeleteModal = (admin) => {
+        setSelectedAdmin(admin);
+        setConfirmDeleteModalOpen(true);
+    };
+
+    const handleDeleteAdmin = async () => {
+        const res = await dispatch(deleteAdminAction(selectedAdmin?._id));
+
+        if (!res.success) {
+            toast.error(res.message, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+            });
+            setConfirmDeleteModalOpen(false);
+            setSelectedAdmin({});
+            return;
+        }
+        dispatch(getAdminsAction());
+
+        setConfirmDeleteModalOpen(false);
+        toast.success(`Admin Deleted Successfully`, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+        });
+    };
+
+    useEffect(() => {
+        authDetails?._id && dispatch(getAdminsAction());
+    }, [dispatch, authDetails?._id]);
+
     return (
         <DashboardWrapper>
             <Header title="Admins List" />
-            <div className="p-4">
-                <SearchInput />
+            <div className="p-4 ">
+                <div className="flex justify-between items-center">
+                    <SearchInput />
+                    <div
+                        onClick={() => setAddModalOpen(true)}
+                        className="flex cursor-pointer items-center space-x-2 bg-seagreen py-2 px-6 rounded-md text-white  text-sm hover:opacity-75"
+                    >
+                        <HiPlusCircle />
+                        <span>New Admin</span>
+                    </div>
+                </div>
+
+                <div className="mt-5">
+                    <div className="relative shadow-md rounded-md bg-white">
+                        <table className="w-full text-sm text-left overflow-x-auto">
+                            <thead className="text-md bg-dimgray text-white">
+                                <tr>
+                                    <th className="px-6 py-4">#</th>
+                                    <th className="px-6 py-4">First Name</th>
+                                    <th className="px-6 py-4">Last Name</th>
+                                    <th className="px-6 py-4">Email</th>
+                                    <th className="px-6 py-4">Username</th>
+                                    <th className="px-6 py-4">
+                                        <span className="sr-only">Action</span>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {admins?.map((admin, idx) => (
+                                    <tr
+                                        key={admin?._id}
+                                        className="group border-b "
+                                    >
+                                        <td className="px-6 py-4 font-bold">
+                                            {idx + 1}
+                                        </td>
+                                        <td className="px-6 py-4 capitalize">
+                                            {admin?.firstname}
+                                        </td>
+                                        <td className="px-6 py-4 capitalize">
+                                            {admin?.lastname}
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            {admin?.email}
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            {admin?.username}
+                                        </td>
+                                        <td className="px-6  py-4 text-right flex justify-end items-center space-x-1">
+                                            <div
+                                                className=" flex items-center space-x-1 bg-seagreen text-white text-xs p-2 
+                                                rounded-full hover:opacity-90 hover:scale-[1.02] cursor-pointer"
+                                                onClick={() =>
+                                                    openViewModal(admin)
+                                                }
+                                            >
+                                                <FaEye />
+                                            </div>
+
+                                            <div
+                                                className="flex items-center space-x-1 bg-dimgray text-white text-xs p-2 
+                                                rounded-full hover:opacity-90 hover:scale-[1.02] cursor-pointer"
+                                                onClick={() =>
+                                                    openEditModal(admin)
+                                                }
+                                            >
+                                                <FaUserEdit />
+                                            </div>
+
+                                            <div
+                                                className="flex items-center space-x-1 bg-salmon text-white text-xs p-2 
+                                            rounded-full cursor-pointer hover:opacity-90 hover:scale-[1.02]"
+                                                onClick={() =>
+                                                    openDeleteModal(admin)
+                                                }
+                                            >
+                                                <FaTrash />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+
+                        {loading_adm && (
+                            <div
+                                colSpan={5}
+                                className="py-4 flex justify-center"
+                            >
+                                <FaSpinner className="animate-spin  text-lg text-slate-900" />
+                            </div>
+                        )}
+                    </div>
+
+                    <div className="flex my-4 justify-between">
+                        <p>Showing 1-10 out of 50 </p>
+                        <span>Pagination</span>
+                    </div>
+                </div>
+                <ConfirmDeleteModal
+                    isOpen={confirmDeleteModalOpen}
+                    closeModal={() => {
+                        setConfirmDeleteModalOpen(false);
+                        setSelectedAdmin({});
+                    }}
+                    message="Deleting the admin will erase everything about their
+                        records. Are you sure you want to delete?"
+                    actionMethod={handleDeleteAdmin}
+                    loading={deleting_adm}
+                />
             </div>
         </DashboardWrapper>
     );
