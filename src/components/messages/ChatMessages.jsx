8e87f40b8e4db48cfab5134f9895e447072cd853
Martin Mwangi <mwangimartin1904@gmail.com>
2022-05-03 14:22:14 +0300

    :sparkles: updates

--- src/components/messages/ChatMessages.jsx
+++ src/components/messages/ChatMessages.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const ChatMessages = () => {
   return (
-	<div className='flex-[1] px-5'>ChatMessages</div>
+	  <div className='flex-[1] px-5 flex flex-col h-full'>
+		  
+		  {/* Header */}
+		  <div className="my-5">
+			  <h2>Chat with</h2>
+			  <h1 className='font-bold text-lg'>Martin Kamau</h1>
+		  </div>
+		  <div className="bg-gray-100 p-4 min-h-[600px]"></div>
+		  <div className="py-5 bg-slate-400 mt-5"></div>
+	</div>
   )
 }
 
