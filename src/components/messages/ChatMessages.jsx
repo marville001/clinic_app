568d1fa21541a4eb0bfe48625fc781e7e2623fc0
Martin Mwangi <mwangimartin1904@gmail.com>
2022-05-09 22:41:32 +0300

    :sparkles: updates

--- src/components/messages/ChatMessages.jsx
+++ src/components/messages/ChatMessages.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import ChatContainer from "./ChatContainer";
 import { FaBars, FaPaperPlane, FaSpinner } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
@@ -11,42 +11,62 @@ const ChatMessages = ({ selectedChat, setChatInfoOpen }) => {
     );
     const { authDetails } = useSelector((state) => state.authState);
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        clearErrors,
-        reset
-    } = useForm();
-    const dispatch = useDispatch()
+    const [text, setText] = useState("")
+    const [error, setError] = useState("")
 
-    const handleSendMessage = async (data) => {
+    const dispatch = useDispatch();
 
-        await dispatch(sendMessageAction({
-            chatId: selectedChat?._id,
-            content: data.message
-        }));
+    const handleSendMessage = async (e) => {
+        e.preventDefault();
+        setError("")
 
-        reset();
+        if (text === "") {
+            setError("Please type a message to continue")
+            return;
+        }
+
+        await dispatch(
+            sendMessageAction({
+                chatId: selectedChat?._id,
+                content: text,
+            })
+        );
+
+        setText("")
+
     };
 
-   
+    const typingHandler = (e) => {
+        setError("")
+        setText(e.target.value)
+    };
 
     return (
         <div className="flex-[1] px-5 flex flex-col h-full">
             {/* Header */}
-            {selectedChat?._id &&
+            {selectedChat?._id && (
                 <div className="flex items-center justify-between">
                     <div className="my-5">
                         <h2>Chat with</h2>
                         <h1 className="font-bold text-lg">
-                            {selectedChat?.users?.find(c=>c._id !== authDetails?._id)?.firstname}{" "}
-                            {selectedChat?.users?.find(c=>c._id !== authDetails?._id)?.lastname}
+                            {
+                                selectedChat?.users?.find(
+                                    (c) => c._id !== authDetails?._id
+                                )?.firstname
+                            }{" "}
+                            {
+                                selectedChat?.users?.find(
+                                    (c) => c._id !== authDetails?._id
+                                )?.lastname
+                            }
                         </h1>
                     </div>
-                    <FaBars onClick={()=>setChatInfoOpen(true)} className="text-lg cursor-pointer opacity-70 lg:hidden" />
+                    <FaBars
+                        onClick={() => setChatInfoOpen(true)}
+                        className="text-lg cursor-pointer opacity-70 lg:hidden"
+                    />
                 </div>
-            }
+            )}
             <div className="flex-[1] overflow-scroll no-scroll">
                 {messages?.map((chat) => (
                     <React.Fragment key={chat._id}>
@@ -83,28 +103,21 @@ const ChatMessages = ({ selectedChat, setChatInfoOpen }) => {
                     )}
             </div>
             <form
-                onSubmit={handleSubmit(handleSendMessage)}
+                onSubmit={handleSendMessage}
                 autoComplete="off"
                 className="flex items-center gap-3 mt-5"
             >
                 <div className="h-12 w-full p-1 ring-1 relative rounded-full">
-                    {errors.message && (
+                    {error && (
                         <div className="bg-gray-200 p-3 py-1 text-xs absolute top-0 -translate-y-full left-5 rounded-t-lg right-5 ">
-                            {errors.message.message}
+                            {error}
                         </div>
                     )}
                     <input
                         type="text"
                         disabled={!selectedChat?._id}
-                        {...register("message", {
-                            required: {
-                                value: true,
-                                message: `Please type a message to continue`,
-                            },
-                        })}
-                        onChange={() => {
-                            clearErrors()
-                        }}
+                        value={text}
+                        onChange={typingHandler}
                         placeholder="Type message here."
                         className="w-full h-full disabled:cursor-not-allowed bg-transparent rounded-full outline-none ring-0 focus:ring-0 focus:outline-none focus:border-0 border-0"
                     />
