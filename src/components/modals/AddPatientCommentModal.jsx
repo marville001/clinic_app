3ef51e04d228d3d68b64363e7c5b5434efa237b6
Martin Mwangi <mwangimartin1904@gmail.com>
2022-05-21 17:56:46 +0300

    :sparkles: updates

--- src/components/modals/AddPatientCommentModal.jsx
+++ src/components/modals/AddPatientCommentModal.jsx
@@ -7,146 +7,141 @@ import Modal from "../common/Modal";
 import SelectField from "../common/SelectField";
 import { toast } from "react-toastify";
 import {
-  createCommentAction,
-  createContactAction,
-  getCommentsAction
+    createCommentAction,
+    getCommentsAction,
 } from "../../redux/actions/patients.action";
 import { useParams } from "react-router-dom";
 
 const AddPatientCommentModal = ({
-  isOpen = false,
-  closeModal = () => {},
-  loading,
+    isOpen = false,
+    closeModal = () => {},
+    loading,
 }) => {
-  const { authDetails } = useSelector(
-    (state) => state.authState
-  );
-  const { commentType, creatingComment } = useSelector(
-    (state) => state.patientsState
-  );
-
-
-  const [error, setError] = useState("");
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-    clearErrors,
-  } = useForm();
-
-  const dispatch = useDispatch();
-
-  const handleCloseModal = () => {
-    reset();
-    clearErrors();
-    closeModal();
-    setError("");
-  };
-
-  const { id } = useParams();
-
-  const handleAddComment = async (data) => {
-    setError("");
-
-    const { type, comment, ...rest } = data;
-    const res = await dispatch(
-      createCommentAction(
-        {
-          ...rest,
-          comment,
-          commenttype:type,
-          senderRole: authDetails.role,
-          senderId: authDetails._id
-        },
-        id
-      )
+    const { authDetails } = useSelector((state) => state.authState);
+    const { commentType, creatingComment } = useSelector(
+        (state) => state.patientsState
     );
 
-    if (!res.success) {
-      setError(res.message);
-      return;
-    }
+    const [error, setError] = useState("");
 
-    toast.success(`Comment Added Successfully`, {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-    });
-    handleCloseModal();
-    authDetails?._id && dispatch(getCommentsAction(id));
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+        clearErrors,
+    } = useForm();
 
-  };
+    const dispatch = useDispatch();
 
-  return (
-    <Modal size="xl" isOpen={isOpen} closeModal={handleCloseModal}>
-      <form
-        onSubmit={handleSubmit(handleAddComment)}
-        className="bg-white p-5 _shadow rounded-md"
-      >
-        <h4 className="text-center text-2xl text-slate-900 mb-6">
-          Add Comment
-        </h4>
+    const handleCloseModal = () => {
+        reset();
+        clearErrors();
+        closeModal();
+        setError("");
+    };
 
-        {error && (
-          <div className="text-center bg-red-200 rounded-md text-red-500 my-4 text-sm p-1">
-            {error}
-          </div>
-        )}
+    const { id } = useParams();
 
-        <div className="flex  flex-col gap-5 mt-4">
-          <SelectField
-            errors={errors}
-            name="type"
-            label="Comment Type"
-            register={register}
-            required={true}
-            options={commentType.map((type) => {
-              return {
-                value: type._id,
-                label: type.name,
-              };
-            })}
-          />
-          <InputField
-            errors={errors}
-            name="comment"
-            label="Comment"
-            register={register}
-            required={true}
-            type="text"
-          />
-        </div>
+    const handleAddComment = async (data) => {
+        setError("");
 
-        <div className="flex justify-between items-center mt-8">
-          <button
-            type="button"
-            onClick={handleCloseModal}
-            className="bg-salmon rind-0 border-0 outline-none text-white py-2 px-5 rounded-md"
-          >
-            No
-          </button>
-          <button
-            disabled={creatingComment}
-            className="disabled:opacity-50 disabled:cursor-not-allowed uppercase px-16
+        const { type, comment, ...rest } = data;
+        const res = await dispatch(
+            createCommentAction(
+                {
+                    ...rest,
+                    comment,
+                    commenttype: type,
+                    senderRole: authDetails.role,
+                    senderId: authDetails._id,
+                },
+                id
+            )
+        );
+
+        if (!res.success) {
+            setError(res.message);
+            return;
+        }
+
+        toast.success(`Comment Added Successfully`, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+        });
+        handleCloseModal();
+        authDetails?._id && dispatch(getCommentsAction(id));
+    };
+
+    return (
+        <Modal size="xl" isOpen={isOpen} closeModal={handleCloseModal}>
+            <form
+                onSubmit={handleSubmit(handleAddComment)}
+                className="bg-white p-5 _shadow rounded-md"
+            >
+                <h4 className="text-center text-2xl text-slate-900 mb-6">
+                    Add Comment
+                </h4>
+
+                {error && (
+                    <div className="text-center bg-red-200 rounded-md text-red-500 my-4 text-sm p-1">
+                        {error}
+                    </div>
+                )}
+
+                <div className="flex  flex-col gap-5 mt-4">
+                    <SelectField
+                        errors={errors}
+                        name="type"
+                        label="Comment Type"
+                        register={register}
+                        required={true}
+                        options={commentType.map((type) => {
+                            return {
+                                value: type._id,
+                                label: type.name,
+                            };
+                        })}
+                    />
+                    <InputField
+                        errors={errors}
+                        name="comment"
+                        label="Comment"
+                        register={register}
+                        required={true}
+                        type="text"
+                    />
+                </div>
+
+                <div className="flex justify-between items-center mt-8">
+                    <button
+                        type="button"
+                        onClick={handleCloseModal}
+                        className="bg-salmon rind-0 border-0 outline-none text-white py-2 px-5 rounded-md"
+                    >
+                        No
+                    </button>
+                    <button
+                        disabled={creatingComment}
+                        className="disabled:opacity-50 disabled:cursor-not-allowed uppercase px-16
 						 tracking-wider py-2 text-white text-lg rounded-md flex items-center
 						 bg-seagreen
                      "
-          >
-            {creatingComment ? (
-              <FaSpinner className="animate-spin mr-4" />
-            ) : (
-              "Add Comment"
-            )}
-          </button>
-        </div>
-      </form>
-    </Modal>
-  );
+                    >
+                        {creatingComment ? (
+                            <FaSpinner className="animate-spin mr-4" />
+                        ) : (
+                            "Add Comment"
+                        )}
+                    </button>
+                </div>
+            </form>
+        </Modal>
+    );
 };
 
 export default AddPatientCommentModal;
